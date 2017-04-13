9fb8bb5957e7eb995d159a0c92018e7c09131437
FFTi <jfang8@stevens.edu>
2017-04-13 10:29:22 -0400

    Add files via upload

---
+++ routes/login.js
@@ -0,0 +1,61 @@
+const express = require('express');
+const router = express.Router();
+const data = require("../data");
+const User = data.users;
+const path = require('path');
+const passport = require('passport');
+const LocalStrategy = require('passport-local').Strategy;
+
+passport.use(new LocalStrategy({
+    usernameField: 'username',
+    passwordField: 'password'
+},
+    function (username, password, done) {
+
+        User.getUserByUsernameAndPassword(username, password).then((user) => {
+            return done(null, user);
+        }, (reject) => {
+            return done(null, false);
+        });
+    }
+));
+
+passport.serializeUser(function (user, done) {
+    done(null, user);
+});
+
+passport.deserializeUser(function (user, done) {
+    done(null, user);
+});
+
+router.get("/", (req, res) => {
+
+    if (req.user) {
+        res.redirect("private");
+    }
+    else {
+        res.render("layouts/login",{ message: "Please input your username and password!"});
+    }
+});
+
+router.get("/private", (req, res, next) => {
+    if (!req.user) {
+        res.render("layouts/login", { message: "Please Login Firstly" });
+    }
+    else {
+        res.render("layouts/private", req.user);
+    }
+});
+
+
+router.post('/login',
+    passport.authenticate('local', {
+        successRedirect: '/private',
+        successFlash: 'Welcome!',
+        failureRedirect: '/',
+        failureFlash: 'Invalid username or password.',
+        failureFlash: true
+    })
+);
+
+module.exports = router;
