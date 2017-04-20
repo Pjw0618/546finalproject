74c8817414a27723d276fdbc63a12c13e0087386
HanchaoLi <seraphme1234@gmail.com>
2017-04-20 19:25:41 -0400

    ddd

--- routes/register.js
+++ routes/register.js
@@ -1,3 +1,4 @@
+
 const express = require('express');
 const router = express.Router();
 const data = require("../data");
@@ -7,44 +8,14 @@ const path = require('path');
 console.log("register");
 
 router.post("/register", (req, res) => {
-  Register.registerUser(username, password, reenterpassword).then(() => {
     Register.registerUser(req.body.username, req.body.password, req.body.reenterpassword).then(() => {
 
       res.render("layouts/login");
       res.render("layouts/profile");
 
-<<<<<<< HEAD
-
-    // if(!(req.body.username.length >= 6 && req.body.username.length <= 10)){
-    // 	console.log(req.body.username);
-
-    // 	res.render("layouts/register",{message:"Please enter your username and length must between 6 to 10"});
-    // }else if(req.body.password.length <= 0){
-    // 	console.log(req.body.password);
-
-    // 	res.render("layouts/register",{message:"Please enter your password"});
-    // }else if(req.body.reenterpassword <= 0 ){
-    // 	console.log(req.body.reenterpassword);
-    // 	res.render("layouts/register",{message:"Please re-enter your password"});
-    // }else if(req.body.password <= 0 && req.body.password===req.body.reenterpassword){
-
-    // 	res.render("layouts/register",{message:"your password does not match the first time you enter"});
-    // }else{
-    // 	//db operation, insert data to db
-    // 	res.render("layouts/login");
-    // }
-    console.log(req);
-    Register.registerUser(req.body.username,req.body.password,req.body.reenterpassword).then(()=>{
-    	
-		res.render("layouts/profile");
-
-
-    },(reject)=>{
-
-    	res.render("layouts/register",{message: `${reject}`});
-
+    }, (reject) => {
+      res.render("layouts/register", { message: `${reject}` });
     });
   })
-})
 
 module.exports = router;
